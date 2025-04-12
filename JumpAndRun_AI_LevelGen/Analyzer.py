28ba633385622decf5451c2b58f4a2d3069aaa52
LenaEwerling <lena.e2000@web.de>
2025-04-12 09:29:56 +0200

    Analysis extended

--- JumpAndRun_AI_LevelGen/Analyzer.py
+++ JumpAndRun_AI_LevelGen/Analyzer.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 #import mathplotlib as plt
 import os
 import json
+import random
 
 DATA_FILE = "data/game_data.csv"
 PARAMETER_FILE = "parameter.json"
@@ -22,28 +24,60 @@ class Analyzer:
         if os.path.isfile(DATA_FILE):
             self.data_file = pd.read_csv(DATA_FILE)
 
-    def adjust_difficulty(self):
-        print("adjust_difficulty")
-        if not self.data_file.empty:
-            increases = self.data_file['Time_Survived'] / self.data_file['Change_Interval']
-            #increases = [int(row['Time_Survived']) / float(row['Change_Interval']) for row in self.data_file]
-            if not increases.empty:
-                print(f"increases: {increases}")
-                avg_increases = increases.mean()
-                remainder = avg_increases % 1
-                if avg_increases <= 3:
-                    self.params['speed_factor'] -= 0.02
-                    self.params['spawn_factor'] += 0.02
-                elif avg_increases >= 9:
-                    self.params['speed_factor'] += 0.02
-                    self.params['spawn_factor'] -= 0.02
-                print(f"adjusted parameters: {self.params}")
-
     def analyze(self):
         self.load_data()
         self.load_parameters()
         self.adjust_difficulty()
+        print(f"adjusted parameters: {self.params}")
         self.save_parameters()
+
+    def adjust_difficulty(self):
+        if not self.data_file.empty:
+            last_five = self.data_file[['Time_Survived', 'Change_Interval']].tail(5)
+            time_survived = last_five['Time_Survived'].tail(2)
+            avg_time_survived = time_survived.mean()
+            increases = last_five['Time_Survived'] / last_five['Change_Interval']
+            fract_part = increases - np.floor(increases)
+            self.adjust_initial_dificulty(avg_time_survived)
+            self.adjust_factors(fract_part)
+        
+    def adjust_initial_dificulty(self, avg_time_survived):
+        if avg_time_survived == 0.0:
+            print("avg_time_survived invalid")
+            return
+        if avg_time_survived <= 30:                      #to difficult
+            print("to difficult")
+            self.params['speed'] -= 0.02
+            self.params['spawn_interval'] += 0.02
+        elif avg_time_survived >= 90:                    #to easy
+            print("to easy")
+            self.params['speed'] += 0.02
+            self.params['spawn_interval'] -= 0.02
+            probability = random.random()
+            if (probability >= 0.3):
+                print("increase obstacle difficulty")
+            
+
+    def adjust_factors(self, fract_parts):
+        if fract_parts.empty:
+            print("fract_parts invalid")
+            return
+        count_smaller = (fract_parts <= 0.2).sum()
+        count_bigger = (fract_parts >= 0.8).sum()
+        if (count_smaller  >= 3):                   #change is to abrupt
+            print("to abrupt")
+            self.params['speed_factor'] -= 0.02
+            self.params['spawn_factor'] += 0.02
+        elif (count_bigger >= 3):                   #change is to lush
+            print("to lush")
+            self.params['speed_factor'] += 0.02
+            self.params['spawn_factor'] -= 0.02
+
+
+
+
+
+    
  
 
 
